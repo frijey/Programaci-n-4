3801a342e20c5cf075922bc719818a214dcf45b1
Jeffry Marte Bonifacio <frijey@hotmail.com>
2018-02-16 20:46:47 -0400

    Agreagdos

---
+++ Gestion de Equipos/Entrada y Salida de Equipos.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Gestion_de_Equipos
+{
+    public partial class Entrada_y_Salida_de_Equipos : Form
+    {
+        public Entrada_y_Salida_de_Equipos()
+        {
+            InitializeComponent();
+        }
+    }
+}
